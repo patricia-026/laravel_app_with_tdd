0658093526f1b6020e471485db03c33f3ff8c541
Patricia <kovach.patri@gmail.com>
2023-10-20 14:18:49 +0200

    episode 18 - reduce form duplication

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,7 @@ Route::group(['middleware' => 'auth'], function () {
     Route::get('/projects', [ProjectsController::class, 'index']);
     Route::get('/projects/create', [ProjectsController::class, 'create']);
     Route::get('/projects/{project}', [ProjectsController::class, 'show']);
+    Route::get('/projects/{project}/edit', [ProjectsController::class, 'edit']);
     Route::patch('/projects/{project}', [ProjectsController::class, 'update']);
     Route::post('/projects', [ProjectsController::class, 'store']);
 
